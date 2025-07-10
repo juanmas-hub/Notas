5da3fff7253651329ed8d6de1b19ce0deb85c941
Juanma <juancitobravoc@gmail.com>
2025-07-10 00:37:24 -0300

    feat: add tag entity

---
+++ backend/src/main/java/com/bravo/backend/models/Note.java
@@ -0,0 +1,40 @@
+package com.bravo.backend.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data // toString, getters, setter, hashcode, equals
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "note title cannot be blank")
+    @Size(min = 1, max = 50)
+    private String title;
+    private String content;
+
+    private boolean archived = false;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "note_tags",
+            joinColumns = @JoinColumn(name = "note_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+}
---
+++ backend/src/main/java/com/bravo/backend/models/Tag.java
@@ -0,0 +1,24 @@
+package com.bravo.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data // toString, getters, setter, hashcode, equals
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Note> notes = new HashSet<>();
+}
